eebfea555c076389630430629845a810246d3155
champfish <champfish@users.noreply.github.com>
2015-03-19 13:13:56 -0500

    Update MatrixSumming.java

--- matrix_summing/MatrixSumming.java
+++ matrix_summing/MatrixSumming.java
@@ -3,11 +3,7 @@ import java.io.*;
 
 public class MatrixSumming
 {
-    private int[][] m = {
-        {5,6},
-        {7,8},
-        {3,4}
-    };   //load in the matrix values
+    private int[][] m;  //load in the matrix values
 
     public int sum( int r, int c )
     {
