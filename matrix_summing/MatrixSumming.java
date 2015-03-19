c246aede364217cd3a871fabf3ca01b44ef0d9e9
champfish <champfish@users.noreply.github.com>
2015-03-19 13:09:44 -0500

    Update MatrixSumming.java

--- matrix_summing/MatrixSumming.java
+++ matrix_summing/MatrixSumming.java
@@ -3,11 +3,32 @@ import java.io.*;
 
 public class MatrixSumming
 {
-    private int[][] m = {{5,6},{7,8},{3,4}};   //load in the matrix values
+    private int[][] m = {
+        {5,6},
+        {7,8},
+        {3,4}
+    };   //load in the matrix values
 
     public int sum( int r, int c )
     {
-    	return 0;
+    	int sum=0;
+    	for(int r=-1;r<=1;r++)
+    	{
+    	    for(int c=-1;c<=1;c++)
+    	    {
+    	        sum+=getValue(r,c);
+    	    }
+    	}
+    	return sum;
+    }
+    
+    public int getValue(int r, int c)
+    {
+        try
+        {
+            return [r][c];
+        }catch(Exception e){}
+        return 0;
     }
 
     public String toString()
